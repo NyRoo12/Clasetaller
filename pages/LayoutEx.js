8c8f3435fd6a1899a72309386e6cb29bcfc95a32
NyRoo12 <nicolas.donoso@alumnos.uach.cl>
2022-11-09 15:46:49 -0300

    LayoutEx

---
+++ pages/LayoutEx.js
@@ -0,0 +1,19 @@
+
+
+
+const LayoutEx = () => {
+    return (
+        <div id = "BloquePrincipal">
+            <div id = "menuLateral">
+                <a href = "http:www.google.com"> Ir a google</a>
+            </div>
+            <div id = "contenidoPrincipal">
+                
+            </div>
+        </div>
+
+
+    );
+}
+
+export default LayoutEx;
