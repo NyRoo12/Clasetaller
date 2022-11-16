8fc1553f2c20dae048c46676830a63454c929c01
Nicolas Alfonso Donoso Navarrete <nicolas.donoso@alumnos.uach.cl>
2022-11-16 01:18:05 -0300

    Actualizado

--- pages/ejemplo.js
+++ pages/ejemplo.js
@@ -1,7 +1,5 @@
 import {
-  Input,
   Drawer,
-  DrawerBody,
   DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
@@ -9,24 +7,13 @@ import {
   DrawerCloseButton,
   Button,
   useDisclosure,
-  Box,
   Center,
   Flex,
   Spacer,
-  Stack,
-  Badge,
-  Text,
-  Checkbox,
   FormControl,
   FormLabel,
   Switch,
-  ButtonGroup,
-  Alert,
-  AlertIcon,
-  AlertTitle,
-  AlertDescription,
-
-
+  Image,
 }
 
   from "@chakra-ui/react";
@@ -44,11 +31,10 @@ function Ejemplo() {
 
   return (
     <>
-
       <br />
       <Flex>
 
-        <Button ref={btnRef} w='10%' colorScheme="teal" onClick={onOpen} height='43px' fontSize='17px' left='2%' >
+        <Button ref={btnRef} w='10%' colorScheme="blue" onClick={onOpen} height='43px' fontSize='17px' left='2%'  >
           LINEAS
         </Button>
         <br />
@@ -58,13 +44,25 @@ function Ejemplo() {
             <Spacer />
             <Spacer />
             <box>
-              <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d24061.16525358198!2d-73.23897385001462!3d-39.83157661485098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x9615ee9ff311985f%3A0xc0a003eae97d725f!2sAv.%20Balmaceda%2C%20Valdivia%2C%20Los%20R%C3%ADos!3m2!1d-39.8073766!2d-73.20918979999999!4m5!1s0x9615ee4bc884f601%3A0xe3444bee10bfd1f!2zSW5nLiBSYcO6bCBTw6FleiBTw6FleiwgVmFsZGl2aWEsIExvcyBSw61vcw!3m2!1d-39.850179!2d-73.2501039!5e0!3m2!1ses-419!2scl!4v1668395023561!5m2!1ses-419!2scl" width="1500" height="750" allowfullscreen="allowfullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>0
+              <iframe src="https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d24061.16525358198!2d-73.23897385001462!3d-39.83157661485098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e3!4m5!1s0x9615ee9ff311985f%3A0xc0a003eae97d725f!2sAv.%20Balmaceda%2C%20Valdivia%2C%20Los%20R%C3%ADos!3m2!1d-39.8073766!2d-73.20918979999999!4m5!1s0x9615ee4bc884f601%3A0xe3444bee10bfd1f!2zSW5nLiBSYcO6bCBTw6FleiBTw6FleiwgVmFsZGl2aWEsIExvcyBSw61vcw!3m2!1d-39.850179!2d-73.2501039!5e0!3m2!1ses-419!2scl!4v1668395023561!5m2!1ses-419!2scl" width="1500" height="750" allowfullscreen="allowfullscreen" loading="lazy" referrerpolicy="no-referrer-when-downgrade" ></iframe>0
             </box>
           </Center>
+
+          <flex>
+
+            <Image
+              borderRadius='full'
+              borderColor='black'
+              boxSize='100px'
+              objectFit='cover'
+              src='https://cdn.discordapp.com/attachments/990043778817609752/1042285125561499678/image.png'
+              alt='Dan Abramov'
+            />
+
+          </flex>
         </Flex>
-        <br />
-        <br />
       </Flex>
+
       <Drawer
         isOpen={isOpen}
         placement="left"
@@ -82,8 +80,8 @@ function Ejemplo() {
             </FormLabel>
             <Switch size='sm' colorScheme='yellow' />
           </FormControl>
-          <br />
 
+          <br />
           <Button w='65%' colorScheme="blue" left='5%' onClick={() => handleClick("Linea 1")} >
             Linea 1
           </Button>
@@ -124,6 +122,7 @@ function Ejemplo() {
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
+
     </>
 
   );
