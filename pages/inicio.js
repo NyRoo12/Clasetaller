ad2ba2544008e4766b293f7b012d6af84dcb8f76
M4thiassinh <matias.relauquen@alumnos.uach.cl>
2022-12-26 22:53:02 -0300

    ojito

--- pages/inicio.js
+++ pages/inicio.js
@@ -1,9 +1,11 @@
-
+import {FormLabel, Switch} from "@chakra-ui/react";
 import { useState } from "react";
 import Layout from './cuerpo'
 //pagina de presentacion//
 export default function inicio() {
     const[estadoModal1, cambiarEstadomodal1] = useState(true);
+    const toggleSwitch = () => { setIsEnabled(!isEnabled) };
+    const [isEnabled, setIsEnabled] = useState(true);
     return (
         <Layout pageId="lineaxd">
         <div class = 'fondo' >
@@ -22,30 +24,23 @@ export default function inicio() {
             <img src='images\flechaR.png' />
         </div>
 
-        <div class = 'cuadroblanco2' >
-        ㅤMuestra ㅤinformacion de las micros
+        <div class = 'flechaArr'>
+            <img src = 'images\flechaArr.png'/>
         </div>
 
-        <div class="promos">
+        <div class = 'cuadroblanco2' >
+        ㅤMuestra ㅤinformación de las micros
+        </div>
 
-            <div class="promo scale">
-            <div class ="cerrar" onClick={()=> cambiarEstadomodal1(false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1px" height="1px" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-            </div>
-            <div class="circulo"> 1 </div>
-            <div class="deal">
-                <span>LINEA</span>
-            </div>
-            <span class="price">Horario: 06:00 - 19:00</span>
-
-            <img src="images/linea1.jpg" width='100%'></img>
-            <FormLabel
+        <div class = 'cuadroblanco3' >
+        ㅤMuestra el recorrido de las micros en los 2 sentidos
+        </div>
+        <FormLabel
                     htmlFor='isChecked'
                     position="fixed"
                     top="226px"
                     left="30px"
+                    color={'greenyellow'}
                 >Ida</FormLabel>
 
 
@@ -63,7 +58,25 @@ export default function inicio() {
                     position="fixed"
                     top="226px"
                     left="102px"
+                    color={'greenyellow'}
                 >Vuelta</FormLabel>
+
+        <div class="promos">
+
+            <div class="promo scale">
+            <div class ="cerrar" onClick={()=> cambiarEstadomodal1(false)}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="1px" height="1px" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </div>
+            <div class="circulo"> 1 </div>
+            <div class="deal">
+                <span>LINEA</span>
+            </div>
+            <span class="price">Horario: 06:00 - 19:00</span>
+
+            <img src="images/linea1.jpg" width='100%'></img>
+            
 
             </div>
 
